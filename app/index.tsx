217936ac4033a7f94f3cc7c0cfc305c46b9b2c19
DiegoPskz <paszkiewiczdiego28@gmail.com>
2025-05-18 21:53:46 -0500

    Inicio de app

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-red-600">EnlatadosMG</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold">Enlatados MG</Text>
     </View>
   );
 }
